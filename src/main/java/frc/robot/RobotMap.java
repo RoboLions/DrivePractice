eddce217653d2464eabb4d5aa7bf3d1cfdf0aa53
Ava B <avabyrd96@gmail.com>
2021-02-25 15:34:52 -0500

    Flipped Motor Ports

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,8 +7,8 @@ import com.ctre.phoenix.sensors.PigeonIMU;
 public class RobotMap {
     public static final int LEFT_FRONT_PORT = 3;
     public static final int RIGHT_FRONT_PORT = 4;
-    public static final int RIGHT_BACK_PORT = 1;
-    public static final int LEFT_BACK_PORT = 2;
+    public static final int RIGHT_BACK_PORT = 2;
+    public static final int LEFT_BACK_PORT = 1;
 
     public static WPI_TalonFX leftFrontMotor = new WPI_TalonFX(LEFT_FRONT_PORT);
     public static WPI_TalonFX rightFrontMotor = new WPI_TalonFX(RIGHT_FRONT_PORT);
